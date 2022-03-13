ae787045d82f3d6d2e6d67a0b8fb215a65f19c6b
mfaizsyahmi <yaema.vandermerwe@gmail.com>
2022-03-13 17:52:24 +0800

    moved sentence regex match part to standalone fn

--- sentences.js
+++ sentences.js
@@ -7,6 +7,80 @@
  * @prop {number} [t] time compression (0-100)
  */
 
+// https://www.russellgood.com/how-to-convert-audiobuffer-to-audio-file/
+function make_download(abuffer, total_samples, name) {
+
+	// set sample length and rate
+	var duration = abuffer.duration,
+		rate = abuffer.sampleRate,
+		offset = 0;
+
+	// Generate audio file and assign URL
+	var new_file = URL.createObjectURL(bufferToWave(abuffer, total_samples));
+
+	// Make it downloadable
+	var download_link = document.createElement("a");
+	download_link.href = new_file;
+	download_link.download = name;
+	download_link.click();
+}
+
+// Convert an AudioBuffer to a Blob using WAVE representation
+function bufferToWave(abuffer, len) {
+	var numOfChan = abuffer.numberOfChannels,
+		length = len * numOfChan * 2 + 44,
+		buffer = new ArrayBuffer(length),
+		view = new DataView(buffer),
+		channels = [], i, sample,
+		offset = 0,
+		pos = 0;
+  
+	// write WAVE header
+	setUint32(0x46464952);                         // "RIFF"
+	setUint32(length - 8);                         // file length - 8
+	setUint32(0x45564157);                         // "WAVE"
+  
+	setUint32(0x20746d66);                         // "fmt " chunk
+	setUint32(16);                                 // length = 16
+	setUint16(1);                                  // PCM (uncompressed)
+	setUint16(numOfChan);
+	setUint32(abuffer.sampleRate);
+	setUint32(abuffer.sampleRate * 2 * numOfChan); // avg. bytes/sec
+	setUint16(numOfChan * 2);                      // block-align
+	setUint16(16);                                 // 16-bit (hardcoded in this demo)
+  
+	setUint32(0x61746164);                         // "data" - chunk
+	setUint32(length - pos - 4);                   // chunk length
+  
+	// write interleaved data
+	for(i = 0; i < abuffer.numberOfChannels; i++)
+	  channels.push(abuffer.getChannelData(i));
+  
+	while (pos < length) {
+	  for(i = 0; i < numOfChan; i++) {             // interleave channels
+		sample = Math.max(-1, Math.min(1, channels[i][offset])); // clamp
+		sample = (0.5 + sample < 0 ? sample * 32768 : sample * 32767)|0; // scale to 16-bit signed int
+		view.setInt16(pos, sample, true);          // write 16-bit sample
+		pos += 2;
+	  }
+	  offset++                                     // next source sample
+	}
+  
+	// create Blob
+	return new Blob([buffer], {type: "audio/wav"});
+  
+	function setUint16(data) {
+	  view.setUint16(pos, data, true);
+	  pos += 2;
+	}
+  
+	function setUint32(data) {
+	  view.setUint32(pos, data, true);
+	  pos += 4;
+	}
+  }
+  
+
 /**
  * Wrapper for AudioBuffer.
  * Allows you to get buffered audio with VOX modifiers applied.
@@ -120,7 +194,7 @@ class BufferedAudio {
 class VOXSpeaker extends EventTarget {
 	
 	//sentences.txt parser 
-	parseRE = /(?<path>\w+\/)|(?<name>\w+!?|\.|,)(?:\((?<mod1>.*?)\))?|\((?<modall>.*?)\)/g;
+	static parseRE = /(?<path>\w+\/)|(?<name>\w+!?|\.|,)(?:\((?<mod1>.*?)\))?|\((?<modall>.*?)\)/dg;
 	
 	constructor(options) {
 		// calls EventTarget's constructor
@@ -252,7 +326,23 @@ class VOXSpeaker extends EventTarget {
 	clearAudioCache() {
 		this.audioMap.clear();
 	}
-	
+
+	/**
+	 * parses a sentence string into match groups, and nothing more
+	 * @param {string} sentence sentence string to parse
+	 * @returns {RegExpExecArray[]} regex matches
+	 */	
+	static parseSentenceRaw(sentence) {
+		// parse the sentence into parts
+		const parts = [];
+		let m;
+		while (m = VOXSpeaker.parseRE.exec(sentence)) {
+			parts.push(m);
+		}
+		
+		return parts;
+	}
+
 	/**
 	 * @typedef wordListItem
 	 * @prop {string} name 
@@ -265,16 +355,11 @@ class VOXSpeaker extends EventTarget {
 	 */
 	/**
 	 * parses a sentence string
-	 * @param {string} sentence 
+	 * @param {string} sentence sentence string to parse
 	 * @returns {parseSentenceReturnObject}
 	 */
 	parseSentence(sentence)	{
-		// parse the sentence into parts
-		const parts = [];
-		let m;
-		while (m = this.parseRE.exec(sentence)) {
-			parts.push(m);
-		}
+		const parts = VOXSpeaker.parseSentenceRaw(sentence);
 		
 		const wordList = [];
 		let path = "vox/", // default path
